4f5e79b0f198dc5980270aa25a222d60988c85bb
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2024-04-28 18:00:09 +0700

    .

---
+++ connect_db.php
@@ -0,0 +1,15 @@
+<?php
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "bt3");
+
+$con = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+if (!$con)
+    echo "fail";
+else
+    echo "success";
+
+?>
